1f5cbae6a3852aeb5c4b5052816851ff6b546933
Akash <akashparekh98@gmail.com>
2025-09-03 11:27:14 +0530

    Created user table and alembic migrations

---
+++ BE/scripts/test_user_inputs.py
@@ -0,0 +1,10 @@
+from app.db.session import SessionLocal
+from app.models.user import User
+
+db = SessionLocal()
+user = User(email="test@example.com", password="hashedpassword")
+db.add(user)
+db.commit()
+db.refresh(user)
+print(user.id, user.email)
+db.close()
