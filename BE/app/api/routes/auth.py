06047309d435fdb5f005949c6de9e183374de1ce
Akash <akashparekh98@gmail.com>
2025-09-04 15:08:53 +0530

    Added Login Route, create session_id and store it in redis, pass session_id in cookies

--- BE/app/api/routes/auth.py
+++ BE/app/api/routes/auth.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
+from typing import cast
+
 from app.db.session import SessionLocal
 from app.models.user import User
 from app.schemas.user import UserCreate
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
+from app.core.sessions import create_session
 
 router = APIRouter()
 
@@ -31,3 +34,25 @@ def register(user_in: UserCreate, db: Session = Depends(get_db)):
     db.refresh(user)
 
     return {"id": str(user.id), "email": user.email}
+
+@router.post("/login")
+def login(user_in: UserCreate, response: Response, db: Session = Depends(get_db)):
+    #Check if user exists
+    existing_user = db.query(User).filter(User.email == user_in.email).first()
+    if not existing_user or not verify_password(user_in.password, cast(str, existing_user.password)):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    # Create Session ID
+    session_id = create_session(str(existing_user.id))
+
+    # Return HTTPOnly Cookie
+    response.set_cookie(
+        key="session_id",
+        value=session_id,
+        httponly=True,
+        max_age=60,
+        secure=False,
+        samesite="lax"
+    )
+
+    return {"message": "Login successful"}
